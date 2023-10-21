93b6a11da91b1b56ac6329480ae24dde50c17eb6
pujiragil <ragilsg164@gmail.com>
2023-10-21 19:20:23 +0700

    installing & customize catppuccin colorscheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,42 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+		"catppuccin/nvim",
+		name = "catppuccin",
+		opts = {
+			term_colors = true,
+			transparent_background = false,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#0d0f18",
+					mantle = "#0d0f18",
+					crust = "#0d0f18",
+				},
+			},
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+			},
+		},
+	},
 }

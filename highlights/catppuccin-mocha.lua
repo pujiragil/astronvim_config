e643f957828d2ec80f23e1cddc7baf1594bfe952
pujiragil <ragilsg164@gmail.com>
2024-01-04 20:27:24 +0700

    custom highlights only for catppuccin-mocha

---
+++ highlights/catppuccin-mocha.lua
@@ -0,0 +1,6 @@
+return {
+  DashboardHeader = { fg = "#caa5f6" },
+  DashboardButtons = { fg = "white", bg = "white" },
+  NeoTreeDirectoryIcon = { fg = "#caa5f6" },
+  NeoTreeDirectoryName = { fg = "#caa5f6" },
+}
